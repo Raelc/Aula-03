cdc851d46d4c99419330ae16effa75a756b7f1bf
Raelc <ichagas496@gmail.com>
2019-06-02 17:55:32 -0300

    mudanças

---
+++ 004if/resposta004if.js
@@ -0,0 +1,15 @@
+var idade= 15
+
+if(idade>0 && idade<=11) {
+
+    console.log("criança")
+} else if(idade>=12 && idade<=18) {
+
+    console.log("adolecente")
+} else if(idade>=19 && idade<=40) {
+
+    console.log("adulto")
+} else if (idade>41) {
+
+    console.log("velho")
+}
